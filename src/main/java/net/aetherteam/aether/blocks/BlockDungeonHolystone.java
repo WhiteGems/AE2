52ad09da240b31893e72dd291cfe5819cb9e18d7
crafteverywhere <1196377271@qq.com>
2013-06-11 18:42:44 +0900

    Update BlockDungeonHolystone.java

--- src/main/java/net/aetherteam/aether/blocks/BlockDungeonHolystone.java
+++ src/main/java/net/aetherteam/aether/blocks/BlockDungeonHolystone.java
@@ -3,6 +3,8 @@ package net.aetherteam.aether.blocks;
 import java.util.HashMap;
 import java.util.List;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.aetherteam.aether.dungeons.DungeonHandler;
 import net.aetherteam.aether.items.AetherItems;
 import net.minecraft.block.Block;
@@ -19,8 +21,8 @@ import net.minecraft.world.World;
 
 public class BlockDungeonHolystone extends BlockAether implements IAetherBlock
 {
-    private HashMap icons = new HashMap();
-    public static final String[] names = new String[]{"Dungeon Holystone", "Dungeon Mossy Holystone"};
+    private Icon dungeonholystone;
+    private Icon dungeonholymossystone;
 
     protected BlockDungeonHolystone(int var1)
     {
@@ -42,23 +44,29 @@ public class BlockDungeonHolystone extends BlockAether implements IAetherBlock
     /**
      * From the specified side and block metadata retrieves the blocks texture. Args: side, metadata
      */
-    public Icon getIcon(int var1, int var2)
+    @Override
+    @SideOnly(Side.CLIENT)
+    public Icon getIcon(int side, int meta)
     {
-        ItemStack var3 = new ItemStack(AetherBlocks.DungeonHolystone, 1, var2);
-        String var4 = var3.getItem().getItemDisplayName(var3);
-        return (Icon) this.icons.get(var4);
+    	switch(meta)
+    	{
+    	case 1:return this.dungeonholystone;
+    	case 3:return this.dungeonholymossystone;
+    	}
+ 
+        return this.dungeonholystone;
     }
 
     /**
      * When this method is called, your block should register all the icons it needs with the given IconRegister. This
      * is the only chance you get to register icons.
      */
-    public void registerIcons(IconRegister var1)
+    @Override
+	@SideOnly(Side.CLIENT)
+    public void registerIcons(IconRegister ir)
     {
-        for (int var2 = 0; var2 < names.length; ++var2)
-        {
-            this.icons.put(names[var2], var1.registerIcon("Aether:" + names[var2]));
-        }
+    	dungeonholystone = ir.registerIcon("Aether:Dungeon Holystone");
+    	dungeonholymossystone = ir.registerIcon("Aether:Dungeon Mossy Holystone");
     }
 
     /**
@@ -81,24 +89,28 @@ public class BlockDungeonHolystone extends BlockAether implements IAetherBlock
                     var2.addStat(StatList.mineBlockStatArray[this.blockID], 1);
                     var7 = new ItemStack(AetherBlocks.Holystone.blockID, 2, 1);
                     this.dropBlockAsItem_do(var1, var3, var4, var5, var7);
-                } else
+                }
+                else
                 {
                     var7 = new ItemStack(AetherBlocks.Holystone.blockID, 1, 1);
                     this.dropBlockAsItem_do(var1, var3, var4, var5, var7);
                 }
-            } else if (var6 == 2)
+            }
+            else if (var6 == 2)
             {
                 if (var2.getCurrentEquippedItem() != null && var2.getCurrentEquippedItem().itemID == AetherItems.SkyrootPickaxe.itemID)
                 {
                     var2.addStat(StatList.mineBlockStatArray[this.blockID], 1);
                     var7 = new ItemStack(AetherBlocks.Holystone.blockID, 2, 3);
                     this.dropBlockAsItem_do(var1, var3, var4, var5, var7);
-                } else
+                }
+                else
                 {
                     var7 = new ItemStack(AetherBlocks.Holystone.blockID, 1, 3);
                     this.dropBlockAsItem_do(var1, var3, var4, var5, var7);
                 }
-            } else
+            }
+            else
             {
                 var7 = new ItemStack(AetherBlocks.Holystone.blockID, 1, var6);
                 this.dropBlockAsItem_do(var1, var3, var4, var5, var7);
@@ -116,7 +128,7 @@ public class BlockDungeonHolystone extends BlockAether implements IAetherBlock
      */
     public void onBlockAdded(World var1, int var2, int var3, int var4)
     {
-        if (DungeonHandler.instance().getInstanceAt(MathHelper.floor_double((double) var2), MathHelper.floor_double((double) var3), MathHelper.floor_double((double) var4)) == null)
+        if (DungeonHandler.instance().getInstanceAt(MathHelper.floor_double((double)var2), MathHelper.floor_double((double)var3), MathHelper.floor_double((double)var4)) == null)
         {
             var1.setBlockToAir(var2, var3, var4);
         }
