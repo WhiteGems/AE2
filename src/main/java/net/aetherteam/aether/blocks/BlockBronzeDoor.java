750afcf2a47d0d4d8569feaeb9b726308ddeb4ef
crafteverywhere <1196377271@qq.com>
2013-06-11 18:49:50 +0800

    Update BlockBronzeDoor.java

--- src/main/java/net/aetherteam/aether/blocks/BlockBronzeDoor.java
+++ src/main/java/net/aetherteam/aether/blocks/BlockBronzeDoor.java
@@ -2,11 +2,9 @@ package net.aetherteam.aether.blocks;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
-
 import net.aetherteam.aether.dungeons.Dungeon;
 import net.aetherteam.aether.dungeons.DungeonHandler;
 import net.aetherteam.aether.party.Party;
@@ -24,8 +22,7 @@ import net.minecraft.world.World;
 public class BlockBronzeDoor extends BlockAether implements IAetherBlock
 {
     private Random rand = new Random();
-    private HashMap icons = new HashMap();
-    public static final String[] names = new String[]{"Bronze Door", "Bronze Door Lock"};
+    private Icon door,lock;
 
     protected BlockBronzeDoor(int var1)
     {
@@ -44,12 +41,13 @@ public class BlockBronzeDoor extends BlockAether implements IAetherBlock
      */
     public void onBlockAdded(World var1, int var2, int var3, int var4)
     {
-        Dungeon var5 = DungeonHandler.instance().getInstanceAt(MathHelper.floor_double((double) var2), MathHelper.floor_double((double) var3), MathHelper.floor_double((double) var4));
+        Dungeon var5 = DungeonHandler.instance().getInstanceAt(MathHelper.floor_double((double)var2), MathHelper.floor_double((double)var3), MathHelper.floor_double((double)var4));
 
         if (var5 != null)
         {
             super.onBlockAdded(var1, var2, var3, var4);
-        } else
+        }
+        else
         {
             var1.setBlock(var2, var3, var4, 0);
         }
@@ -66,11 +64,16 @@ public class BlockBronzeDoor extends BlockAether implements IAetherBlock
     /**
      * From the specified side and block metadata retrieves the blocks texture. Args: side, metadata
      */
-    public Icon getIcon(int var1, int var2)
+    public Icon getIcon(int side, int meta)
     {
-        ItemStack var3 = new ItemStack(AetherBlocks.BronzeDoor, 1, var2);
-        String var4 = var3.getItem().getItemDisplayName(var3);
-        return (Icon) this.icons.get(var4);
+
+        switch(meta)
+    	{
+    	case 0:return this.door;
+    	case 1:return this.lock;
+    	}
+    	
+    	return this.door;
     }
 
     @SideOnly(Side.CLIENT)
@@ -79,12 +82,10 @@ public class BlockBronzeDoor extends BlockAether implements IAetherBlock
      * When this method is called, your block should register all the icons it needs with the given IconRegister. This
      * is the only chance you get to register icons.
      */
-    public void registerIcons(IconRegister var1)
+    public void registerIcons(IconRegister ir)
     {
-        for (int var2 = 0; var2 < names.length; ++var2)
-        {
-            this.icons.put(names[var2], var1.registerIcon("Aether:" + names[var2]));
-        }
+    	door = ir.registerIcon("Aether:Bronze Door");
+    	lock = ir.registerIcon("Aether:Bronze Door Lock");  
     }
 
     /**
@@ -100,7 +101,7 @@ public class BlockBronzeDoor extends BlockAether implements IAetherBlock
                 {
                     if (var1.getBlockId(var10, var11, var12) == AetherBlocks.BronzeDoorController.blockID)
                     {
-                        TileEntityBronzeDoorController var13 = (TileEntityBronzeDoorController) var1.getBlockTileEntity(var10, var11, var12);
+                        TileEntityBronzeDoorController var13 = (TileEntityBronzeDoorController)var1.getBlockTileEntity(var10, var11, var12);
 
                         if (var13 != null)
                         {
