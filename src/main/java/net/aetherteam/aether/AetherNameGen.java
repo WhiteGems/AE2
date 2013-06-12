f84cf785acf23c6ea5852eb8ed1f1dfbbedf0360
pa001024 <pa001024@qq.com>
2013-06-13 00:54:12 +0800

    E 将名称导出txt文件

--- src/main/java/net/aetherteam/aether/AetherNameGen.java
+++ src/main/java/net/aetherteam/aether/AetherNameGen.java
@@ -1,35 +1,65 @@
 package net.aetherteam.aether;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class AetherNameGen
 {
     public static String[] valkNamePrefix = {"Har", "Her", "Gon", "Sko", "Hil"};
-    public static String[] valkNameMiddix = {"fjo", "ska", "bri", "", ""};
+    public static String[] valkNameMiddix = {""};
     public static String[] valkNameSuffix = {"tur", "pul", "dul", "gul", "or"};
 
-    public static String[] name1 = {"毁灭的","狂暴的","毒辣的","邪恶的","诡异的","超能的","狂拽的","浪气的","黑暗的","暗黑的","狂怒的","贪婪的","傲慢的","虚荣的","饕餮的","嫉妒的","色欲的","暴怒的","懒惰的","黑化的","无头骑士","三无的","基佬","百合","胸大无脑的","金刚不坏的","独霸天下的","目中无人的","骨骼惊奇的","武学奇才","节操全失的","灭神屠魔的","魔中之魔","醉鬼","游手好闲的","神机妙算的","唯我独尊的","聪明的","可爱的","活泼的","漂亮的","阴狠毒辣的","狡诈阴险的","道貌岸然的","妖魔化身","酷拽狂霸的","魔中之皇","毁天灭地的","开山劈石的","美如冠玉的","雪肤花颜的","国色天香的","闭月羞花的","高手高手高高手","地上无敌的","残暴无情的","灭绝人寰的","人间劫难","东方不败的","独闯天涯的","无情无义的","冷面杀神","冲天黑煞的","屠杀者","恨天者","补天无力的","无尽轮回的","绝境之","普通的","一般的","平凡的","没什么出奇的","佛面魔心的","上天入地的","色中狂魔","人间失格的","食节操魔","狂拽炫酷的","独孤求败的","梦中恶魔","猩猩队长","遵纪守法的","见义勇为的","拾金不昧的","肌肉化身的","魔神之子", "富甲一方的", "业务繁忙的", "膝盖中箭的", "无辜躺枪的", "科学教徒", "打假卫士", "极品", "力大无穷", "嚣张的", "人见人爱","拉风的","Too young,too naive的","查水表的","拆迁办的","有关部门负责人","死神代言人","专家","成功人士","英灵附体的","关二上身的","屠狗者","异形","天外飞仙","天外来客","成熟体","究极进化的","幼年体"};
+    public static String[] name1 = {"毁灭的","狂暴的","毒辣的","邪恶的","诡异的","超能的","狂拽的","浪气的","黑暗的","暗黑的","狂怒的","贪婪的","傲慢的","虚荣的","饕餮的","嫉妒的","色欲的","暴怒的","懒惰的","黑化的","无头骑士的"};
 
-    public static String[] name2 = {"pa001023","sun","waidely","zestybaby","craft","zesty","crafteverywhere","Azanor","RichardG","zlainsama","pahimar","Alblaka","鸡排","脆饼","kingbdogz","indeed","fanhua","HyperX","Player_Kuso","木是伊","direwolf20","12dora","郝先生","重生是希望","景景","升天的猫","苍生","comeheres","szszss","希特勒","元首","和尚","籽岷","stevefan","dawn","banner","闪耀之灵","sy","SF","wzp","CaptainSparklez","邦莫","米二","zero","ztcjohn","某猫","蓝色铁卷门","大作之月","disco","黑桐谷歌","敖厂长","CovertJaguar","SirSengir","eloraam","大刘","当风过时","小木","kitune","Lawrence","yuxuanchiadm","Qrox","johnbanq","爱丽丝","hendyzone","jcku","tesso","华子","随风澈","WJMZ8MR","天命小猪","卧室小强","矮人","Littlebear","powercrystals","鳄鱼软趴趴","Forever_小树","719823597","outsidero","miguo","DJXGAME"};
+    public static String[] name2 = {"CaptainSparklez","zero","ztcjohn","某猫","蓝色铁卷门","大作之月","disco","黑桐谷歌","敖厂长","CovertJaguar","SirSengir","eloraam"};
 
+    static Random rand = new Random();
 
+    static {
+        String[] temp;
+        temp = loadNames("/names/perfix.txt");
+        if (temp != null) name1 = temp;
+        temp = loadNames("/names/suffix.txt");
+        if (temp != null) name2 = temp;
+        temp = loadNames("/names/valkperfix.txt");
+        if (temp != null) valkNamePrefix = temp;
+        temp = loadNames("/names/valkmiddix.txt");
+        if (temp != null) valkNameMiddix = temp;
+        temp = loadNames("/names/valksuffix.txt");
+        if (temp != null) valkNameSuffix = temp;
+    }
+    private static String[] loadNames(String resname){
+        ArrayList<String> result = new ArrayList();
+        InputStreamReader reader;
+        try
+        {
+            reader = new InputStreamReader(AetherNameGen.class.getResourceAsStream(resname));
+            BufferedReader buf = new BufferedReader(reader);
+            String line;
+            while((line = buf.readLine()) != null) {
+                result.add(line);
+            }
+            buf.close();
+            reader.close();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return (String[]) result.toArray();
+    }
     public static String gen()
     {
-        Random rand = new Random();
         String name = name1[rand.nextInt(name1.length)];
         return name + name2[rand.nextInt(name2.length)];
     }
 
     public static String valkGen()
     {
-        Random rand = new Random();
-        String result = "";
-        // result = result + valkNamePrefix[rand.nextInt(valkNamePrefix.length)];
-        // result = result + valkNameMiddix[rand.nextInt(valkNameMiddix.length)];
-        // result = result + valkNameSuffix[rand.nextInt(valkNameSuffix.length)];
-
-        result = name1[rand.nextInt(name1.length)];
-        result += name2[rand.nextInt(name2.length)];
+        String result;
+        result = valkNamePrefix[rand.nextInt(valkNamePrefix.length)];
+        result += valkNameSuffix[rand.nextInt(valkNameMiddix.length)];
+        result += valkNameSuffix[rand.nextInt(valkNameSuffix.length)];
         return result;
     }
 }
