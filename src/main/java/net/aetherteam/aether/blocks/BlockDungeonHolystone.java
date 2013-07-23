5a2aab4b21e0b3507caf8fa7a43fa5bc221e12c4
crafteverywhere <1196377271@qq.com>
2013-07-23 23:23:50 +0800

    F 贴图注册

--- src/main/java/net/aetherteam/aether/blocks/BlockDungeonHolystone.java
+++ src/main/java/net/aetherteam/aether/blocks/BlockDungeonHolystone.java
@@ -2,6 +2,9 @@ package net.aetherteam.aether.blocks;
 
 import java.util.HashMap;
 import java.util.List;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.aetherteam.aether.dungeons.DungeonHandler;
 import net.aetherteam.aether.items.AetherItems;
 import net.minecraft.block.Block;
@@ -18,8 +21,8 @@ import net.minecraft.world.World;
 
 public class BlockDungeonHolystone extends BlockAether implements IAetherBlock
 {
-    private HashMap icons = new HashMap();
-    public static final String[] names = new String[] {"Dungeon Holystone", "Dungeon Mossy Holystone"};
+    private Icon dungeonholystone;
+    private Icon dungeonholymossystone;
 
     protected BlockDungeonHolystone(int var1)
     {
@@ -41,23 +44,29 @@ public class BlockDungeonHolystone extends BlockAether implements IAetherBlock
     /**
      * From the specified side and block metadata retrieves the blocks texture. Args: side, metadata
      */
-    public Icon getIcon(int var1, int var2)
+    @Override
+    @SideOnly(Side.CLIENT)
+    public Icon getIcon(int side, int meta)
     {
-        ItemStack var3 = new ItemStack(AetherBlocks.DungeonHolystone, 1, var2);
-        String var4 = var3.getItem().getItemDisplayName(var3);
-        return (Icon)this.icons.get(var4);
+        switch(meta)
+    	{
+    	case 1:return this.dungeonholystone;
+    	case 3:return this.dungeonholymossystone;
+    	}
+ 
+        return this.dungeonholystone;
     }
 
     /**
      * When this method is called, your block should register all the icons it needs with the given IconRegister. This
      * is the only chance you get to register icons.
      */
-    public void registerIcons(IconRegister var1)
+    @Override
+	@SideOnly(Side.CLIENT)
+    public void registerIcons(IconRegister ir)
     {
-        for (int var2 = 0; var2 < names.length; ++var2)
-        {
-            this.icons.put(names[var2], var1.registerIcon("Aether:" + names[var2]));
-        }
+    	dungeonholystone = ir.registerIcon("Aether:Dungeon Holystone");
+    	dungeonholymossystone = ir.registerIcon("Aether:Dungeon Mossy Holystone");
     }
 
     /**
