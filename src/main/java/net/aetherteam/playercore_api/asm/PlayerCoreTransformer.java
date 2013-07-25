1852e33a6e70484cc3981bce8814d9ed262add17
pa001024 <pa001024@qq.com>
2013-07-25 21:37:35 +0800

    E/T/F loading coremod

--- src/main/java/net/aetherteam/playercore_api/asm/PlayerCoreTransformer.java
+++ src/main/java/net/aetherteam/playercore_api/asm/PlayerCoreTransformer.java
@@ -1,14 +1,11 @@
 package net.aetherteam.playercore_api.asm;
 
 import cpw.mods.fml.relauncher.IClassTransformer;
-import java.util.ListIterator;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.tree.AbstractInsnNode;
-import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.MethodInsnNode;
-import org.objectweb.asm.tree.MethodNode;
-import org.objectweb.asm.tree.TypeInsnNode;
+import org.objectweb.asm.tree.*;
+
+import java.util.Iterator;
 
 public class PlayerCoreTransformer implements IClassTransformer
 {
@@ -23,97 +20,153 @@ public class PlayerCoreTransformer implements IClassTransformer
     private static final String PLAYERCORE_RENDER = "net.aetherteam.playercore_api.cores.PlayerCoreRender";
     private static final boolean DEBUG_MODE = false;
 
-    public byte[] transform(String var1, String var2, byte[] var3)
+    public byte[] transform(String name, String transformedName, byte[] bytes)
     {
-        ClassReader var4;
-        ClassNode var5;
-        MethodNode var6;
-        TypeInsnNode var7;
-        MethodInsnNode var8;
-        ClassWriter var9;
+        if (name.equals(OBF_RENDERMANAGER) || name.equals(MCP_RENDERMANAGER))
+        {
+            ClassReader classReader = new ClassReader(bytes); ClassNode classNode = new ClassNode();
+            classReader.accept(classNode, 0);
 
-        if (!var1.equals("net.minecraft.client.renderer.entity.RenderManager") && !var1.equals("bgy"))
+            MethodNode methodNode = classNode.methods.get(0);
+
+            TypeInsnNode tn = (TypeInsnNode)methodNode.instructions.get(253);
+            tn.desc = PLAYERCORE_RENDER.replace(".", "/");
+
+            MethodInsnNode mn = (MethodInsnNode)methodNode.instructions.get(255);
+            mn.owner = PLAYERCORE_RENDER.replace(".", "/");
+
+            ClassWriter cw = new ClassWriter(3); classNode.accept(cw);
+
+            return cw.toByteArray();
+        }
+
+        if ( name.equals(OBF_SCM) || name.equals(MCP_SCM))
         {
-            if (!var1.equals("net.minecraft.server.management.ServerConfigurationManager") && !var1.equals("gu"))
+            ClassReader classReader = new ClassReader(bytes); ClassNode classNode = new ClassNode();
+            classReader.accept(classNode, 0);
+
+            MethodNode methodNodeCreatePlayer = classNode.methods.get(12);
+            for (MethodNode m : classNode.methods)
             {
-                if (!var1.equals("net.minecraft.client.multiplayer.PlayerControllerMP") && !var1.equals("bdr"))
+                if (m.name.equals("a") && m.desc.equals("(Ljava/lang/String;)Ljc;"))
                 {
-                    return var3;
-                }
-                else
-                {
-                    var4 = new ClassReader(var3);
-                    var5 = new ClassNode();
-                    var4.accept(var5, 0);
-                    var6 = (MethodNode)var5.methods.get(17);
-                    var7 = (TypeInsnNode)var6.instructions.get(2);
-                    var7.desc = "net.aetherteam.playercore_api.cores.PlayerCoreClient".replace(".", "/");
-                    var8 = (MethodInsnNode)var6.instructions.get(12);
-                    var8.owner = "net.aetherteam.playercore_api.cores.PlayerCoreClient".replace(".", "/");
-                    var9 = new ClassWriter(3);
-                    var5.accept(var9);
-                    return var9.toByteArray();
+                    methodNodeCreatePlayer = m;
+                    System.out.println("analysing methodNodeCreatePlayer, name: " + methodNodeCreatePlayer.name + ", desc: " + methodNodeCreatePlayer.desc);
+                    analyse(methodNodeCreatePlayer);
+                    break;
                 }
             }
-            else
+            int i, c = methodNodeCreatePlayer.instructions.size();
+            for(i = c - 1;i >= 0;i--)
             {
-                var4 = new ClassReader(var3);
-                var5 = new ClassNode();
-                var4.accept(var5, 0);
-                var6 = (MethodNode)var5.methods.get(12);
-                System.out.println("analysing methodNodeCreatePlayer, name: " + var6.name + ", desc: " + var6.desc);
-                this.analyse(var6);
-                var7 = (TypeInsnNode)var6.instructions.get(101);
-                var7.desc = "net.aetherteam.playercore_api.cores.PlayerCoreServer".replace(".", "/");
-                var8 = (MethodInsnNode)var6.instructions.get(112);
-                var8.owner = "net.aetherteam.playercore_api.cores.PlayerCoreServer".replace(".", "/");
-                MethodNode var13 = (MethodNode)var5.methods.get(13);
-                System.out.println("analysing methodNodeRespawnPlayer, name: " + var13.name + ", desc: " + var13.desc);
-                this.analyse(var13);
-                TypeInsnNode var10 = (TypeInsnNode)var13.instructions.get(117);
-                var10.desc = "net.aetherteam.playercore_api.cores.PlayerCoreServer".replace(".", "/");
-                MethodInsnNode var11 = (MethodInsnNode)var13.instructions.get(130);
-                var11.owner = "net.aetherteam.playercore_api.cores.PlayerCoreServer".replace(".", "/");
-                ClassWriter var12 = new ClassWriter(0);
-                var5.accept(var12);
-                return var12.toByteArray();
+                AbstractInsnNode node = methodNodeCreatePlayer.instructions.get(i);
+                if (node instanceof TypeInsnNode && ((TypeInsnNode)node).desc.equals("jc"))
+                {
+                    System.out.println("node: " + ((TypeInsnNode)node).desc);
+                    TypeInsnNode tnCreatePlayer = (TypeInsnNode)node;
+                    tnCreatePlayer.desc = PLAYERCORE_SERVER.replace(".", "/");
+
+                    MethodInsnNode mnCreatePlayer = (MethodInsnNode)methodNodeCreatePlayer.instructions.get(i+11);
+                    mnCreatePlayer.owner = PLAYERCORE_SERVER.replace(".", "/");
+                    break;
+                }
             }
+
+            MethodNode methodNodeRespawnPlayer = classNode.methods.get(13);
+            boolean isMCPC = true;
+            try
+            {
+                Class.forName("org.bukkit.Bukkit");
+            } catch (ClassNotFoundException e)
+            {
+                isMCPC = false;
+            }
+            for (MethodNode m : classNode.methods)
+            {
+                if (isMCPC)
+                {
+                    if (m.name.equals("a") && m.desc.equals("(Ljc;IZ)Ljc;"))
+                    {
+                        methodNodeRespawnPlayer = m;
+                        System.out.println("analysing methodNodeRespawnPlayer, name: " + methodNodeRespawnPlayer.name + ", desc: " + methodNodeRespawnPlayer.desc);
+                        analyse(methodNodeRespawnPlayer);
+                    }
+                } else
+                {
+                    if (m.name.equals("moveToWorld"))
+                    {
+                        methodNodeRespawnPlayer = m;
+                        System.out.println("analysing methodNodeRespawnPlayer, name: " + methodNodeRespawnPlayer.name + ", desc: " + methodNodeRespawnPlayer.desc);
+                        analyse(methodNodeRespawnPlayer);
+                    }
+                }
+            }
+            c = methodNodeRespawnPlayer.instructions.size();
+            for(i = c - 1;i >= 0;i--)
+            {
+                AbstractInsnNode node = methodNodeRespawnPlayer.instructions.get(i);
+                if (node instanceof TypeInsnNode && ((TypeInsnNode)node).desc.equals("jc"))
+                {
+                    TypeInsnNode tnRespawnPlayer = (TypeInsnNode)node;
+                    tnRespawnPlayer.desc = PLAYERCORE_SERVER.replace(".", "/");
+
+                    MethodInsnNode mnRespawnPlayer = (MethodInsnNode)methodNodeRespawnPlayer.instructions.get(i + 13);
+                    mnRespawnPlayer.owner = PLAYERCORE_SERVER.replace(".", "/");
+                    break;
+                }
+            }
+
+            ClassWriter cw = new ClassWriter(0); classNode.accept(cw);
+
+            return cw.toByteArray();
         }
-        else
+
+        if (name.equals(OBF_PLAYERCONTROLLERMP) || name.equals(MCP_PLAYERCONTROLLERMP))
         {
-            var4 = new ClassReader(var3);
-            var5 = new ClassNode();
-            var4.accept(var5, 0);
-            var6 = (MethodNode)var5.methods.get(0);
-            var7 = (TypeInsnNode)var6.instructions.get(253);
-            var7.desc = "net.aetherteam.playercore_api.cores.PlayerCoreRender".replace(".", "/");
-            var8 = (MethodInsnNode)var6.instructions.get(255);
-            var8.owner = "net.aetherteam.playercore_api.cores.PlayerCoreRender".replace(".", "/");
-            var9 = new ClassWriter(3);
-            var5.accept(var9);
-            return var9.toByteArray();
+            ClassReader classReader = new ClassReader(bytes); ClassNode classNode = new ClassNode();
+            classReader.accept(classNode, 0);
+
+            MethodNode methodNode = classNode.methods.get(17);
+
+            TypeInsnNode tn = (TypeInsnNode)methodNode.instructions.get(2);
+            tn.desc = PLAYERCORE_CLIENT.replace(".", "/");
+
+            MethodInsnNode mn = (MethodInsnNode)methodNode.instructions.get(12);
+            mn.owner = PLAYERCORE_CLIENT.replace(".", "/");
+
+            ClassWriter cw = new ClassWriter(3); classNode.accept(cw);
+
+            return cw.toByteArray();
         }
+
+        return bytes;
     }
 
-    public void analyse(MethodNode var1)
+    public void analyse(MethodNode methodNode)
     {
-        ListIterator var2 = var1.instructions.iterator();
+        Iterator iter = methodNode.instructions.iterator(); int i = 0;
 
-        for (int var3 = 0; var2.hasNext(); ++var3)
+        while (iter.hasNext())
         {
-            AbstractInsnNode var4 = (AbstractInsnNode)var2.next();
+            AbstractInsnNode node = (AbstractInsnNode)iter.next();
 
-            if (var4 instanceof TypeInsnNode)
+            if (node instanceof TypeInsnNode)
             {
-                TypeInsnNode var5 = (TypeInsnNode)var4;
-                System.out.println(var3 + ": TypeInsnNode, desc: " + var5.desc);
+                TypeInsnNode tn = (TypeInsnNode)node;
+                System.out.println(i + ": TypeInsnNode, desc: " + tn.desc);
+                continue;
             }
 
-            if (var4 instanceof MethodInsnNode)
+            if (node instanceof MethodInsnNode)
             {
-                MethodInsnNode var6 = (MethodInsnNode)var4;
-                System.out.println(var3 + ": MethodInsnNode, owner: " + var6.owner);
+                MethodInsnNode mn = (MethodInsnNode)node;
+                System.out.println(i + ": MethodInsnNode, owner: " + mn.owner);
+                continue;
             }
+
+            // System.out.println(i + ": Node : " + node.toString());
+
+            i++;
         }
     }
 }
